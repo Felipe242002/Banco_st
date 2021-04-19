39a7ca6a5014c9bb79ad7dcd57525c3979500eb3
FELIPE IGNACIO MONTECINOS MUÑOZ <f.montecinos8@alumnos.santotomas.cl>
2021-04-18 22:36:11 -0400

    Se enviaron los archivos

---
+++ Banco_st/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿
+namespace Banco_st
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNOMBRE = new System.Windows.Forms.TextBox();
+            this.txtAPELLIDO = new System.Windows.Forms.TextBox();
+            this.txtMONTOCREDITO = new System.Windows.Forms.TextBox();
+            this.txtCANTCUOTAS = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnINICIARS = new System.Windows.Forms.Button();
+            this.txtTotalCuot = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtValorC = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(375, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(268, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Banco SantoTomas";
+            // 
+            // txtNOMBRE
+            // 
+            this.txtNOMBRE.Location = new System.Drawing.Point(260, 195);
+            this.txtNOMBRE.Name = "txtNOMBRE";
+            this.txtNOMBRE.Size = new System.Drawing.Size(125, 27);
+            this.txtNOMBRE.TabIndex = 1;
+            // 
+            // txtAPELLIDO
+            // 
+            this.txtAPELLIDO.Location = new System.Drawing.Point(260, 259);
+            this.txtAPELLIDO.Name = "txtAPELLIDO";
+            this.txtAPELLIDO.Size = new System.Drawing.Size(125, 27);
+            this.txtAPELLIDO.TabIndex = 2;
+            // 
+            // txtMONTOCREDITO
+            // 
+            this.txtMONTOCREDITO.Location = new System.Drawing.Point(260, 326);
+            this.txtMONTOCREDITO.Name = "txtMONTOCREDITO";
+            this.txtMONTOCREDITO.Size = new System.Drawing.Size(125, 27);
+            this.txtMONTOCREDITO.TabIndex = 3;
+            // 
+            // txtCANTCUOTAS
+            // 
+            this.txtCANTCUOTAS.Location = new System.Drawing.Point(260, 383);
+            this.txtCANTCUOTAS.Name = "txtCANTCUOTAS";
+            this.txtCANTCUOTAS.Size = new System.Drawing.Size(125, 27);
+            this.txtCANTCUOTAS.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(88, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ingrese su nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(88, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(141, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Ingrese su apellido: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(200, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Monto de credito a solicitar: ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(78, 390);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(144, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Cantidad de cuotas: ";
+            // 
+            // btnINICIARS
+            // 
+            this.btnINICIARS.Location = new System.Drawing.Point(260, 459);
+            this.btnINICIARS.Name = "btnINICIARS";
+            this.btnINICIARS.Size = new System.Drawing.Size(150, 53);
+            this.btnINICIARS.TabIndex = 9;
+            this.btnINICIARS.Text = "Iniciar Simulacion";
+            this.btnINICIARS.UseVisualStyleBackColor = true;
+            this.btnINICIARS.Click += new System.EventHandler(this.btnINICIARS_Click);
+            // 
+            // txtTotalCuot
+            // 
+            this.txtTotalCuot.Location = new System.Drawing.Point(812, 326);
+            this.txtTotalCuot.Name = "txtTotalCuot";
+            this.txtTotalCuot.Size = new System.Drawing.Size(125, 27);
+            this.txtTotalCuot.TabIndex = 10;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(655, 329);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(129, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Valor monto total:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(655, 390);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(122, 20);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Valor cuota total:";
+            // 
+            // txtValorC
+            // 
+            this.txtValorC.Location = new System.Drawing.Point(812, 387);
+            this.txtValorC.Name = "txtValorC";
+            this.txtValorC.Size = new System.Drawing.Size(125, 27);
+            this.txtValorC.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1036, 731);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtValorC);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtTotalCuot);
+            this.Controls.Add(this.btnINICIARS);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCANTCUOTAS);
+            this.Controls.Add(this.txtMONTOCREDITO);
+            this.Controls.Add(this.txtAPELLIDO);
+            this.Controls.Add(this.txtNOMBRE);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNOMBRE;
+        private System.Windows.Forms.TextBox txtAPELLIDO;
+        private System.Windows.Forms.TextBox txtMONTOCREDITO;
+        private System.Windows.Forms.TextBox txtCANTCUOTAS;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnINICIARS;
+        private System.Windows.Forms.TextBox txtTotalCuot;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtValorC;
+    }
+}
+
---
+++ Banco_st/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Banco_st
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnINICIARS_Click(object sender, EventArgs e)
+        {
+
+            int monto = int.Parse(txtMONTOCREDITO.Text);
+            int cuotas = int.Parse(txtCANTCUOTAS.Text);
+            if (monto >= 500000)
+            {
+                if (cuotas >= 1 && cuotas <= 36)
+                {
+                    float interes = 0;
+                    if (cuotas >= 1 && cuotas <= 12)
+                    {
+                        interes = monto * float.Parse("0,1");
+                    }
+                    else if (cuotas >= 13 && cuotas <= 24)
+                    {
+                        interes = monto * float.Parse("0,2");
+                    }
+                    else if (cuotas >= 25 && cuotas <= 36)
+                    {
+                        interes = monto * float.Parse("0,35");
+                    }
+                    float montototal = monto + interes;
+                    float valorcuota = montototal / cuotas;
+                    txtTotalCuot.Text = Convert.ToString(montototal);
+                    txtValorC.Text = Convert.ToString(valorcuota);
+
+                }
+                else
+                {
+                    MessageBox.Show("La cuota debe estar entre 1 y 36 minimo");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Monto minimo de 500000");
+            }
+
+        }
+
+
+    }
+}
